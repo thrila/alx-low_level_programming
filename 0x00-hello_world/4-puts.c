60714ea8b79a91558a230072638a04aa6428d283
Thrila <ifeanyichukwundubuizu@gmail.com>
2022-09-08 16:48:16 -0700

    this is a push for the optional files

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
+
 /**
- * main -Entry point 
- * Description: Prints Programming is like building a multilingual puxxle
- * Return : 0 
- */
+ * main - Entry point
+ * Description: prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
+*/
+
 
 int main(void)
 {
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,12 @@
 
 /**
  * main - Entry point
- * Description : Prints with proper grammar, but the outcome is a piece of art,
- * Return : 0
+ * Description: prints "with proper grammar,but the outcome is a piece of art,"
+ * Return: 0
 */
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
